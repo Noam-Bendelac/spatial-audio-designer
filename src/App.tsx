e39dc81f608d1c7f969e44860c226868c5ec0eac
Noam Bendelac <bendelac.noam@gmail.com>
2022-03-29 18:56:03 -0400

    Add 3D scene

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,79 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef, useState } from 'react'
+import * as THREE from 'three'
+import { Canvas, useFrame } from '@react-three/fiber'
+import logo from './logo.svg'
+import './App.css'
 
-function App() {
+export const App = () => {
+  // pause looping during development for performance
+  const [loop, setLoop] = useState(true)
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="canvas">
+        <Canvas frameloop={loop ? 'always' : 'never'}>
+          <Scene />
+        </Canvas>
+      </div>
+      <div className="sidebar">
+        {/* create-react-app default app */}
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.tsx</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+          <button onClick={() => setLoop(curr => !curr)}>
+            Loop?
+          </button>
+        </header>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+
+// copied from https://github.com/pmndrs/react-three-fiber
+const Box = (props: JSX.IntrinsicElements['mesh']) => {
+  const ref = useRef<THREE.Mesh>(null!)
+  
+  const [hovered, setHovered] = useState(false)
+  const [clicked, setClicked] = useState(false)
+  
+  useFrame(() => {
+    ref.current.rotation.x += 0.01
+    ref.current.rotation.y += 0.01
+  })
+  
+  return (
+    <mesh
+      {...props}
+      ref={ref}
+      scale={clicked ? 1.5 : 1}
+      
+      onClick={() => setClicked(!clicked)}
+      onPointerOver={() => setHovered(true)}
+      onPointerOut={() => setHovered(false)}
+    >
+      <boxGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
+    </mesh>
+  )
+}
+
+const Scene = () => {
+  return <>
+    <ambientLight />
+    <pointLight position={[10, 10, 10]} />
+    <Box position={[-1.2, 0, 0]} />
+    <Box position={[1.2, 0, 0]} />
+  </>
+}
+

31dff0742a2ae12bfc8ba654a1df7e558240d62b
sjl18 <sjl18@vt.edu>
2022-04-03 16:41:17 -0400

    basic elements

--- src/ui/App.tsx
+++ src/ui/App.tsx
@@ -3,6 +3,8 @@ import { Scene } from 'scene3d/Scene'
 import * as model from 'model/model'
 import logo from './logo.svg'
 import styles from './App.module.css'
+import * as React from 'react';
+import objectMenu from './objectMenu'
 
 export const App = () => {
   // pause looping during development for performance
@@ -18,10 +20,31 @@ export const App = () => {
           <p>
             Object Options
           </p>
+          <p className={styles.basic}>X:
+            <input name='x' id='x' type='number' placeholder='X' required/>
+          </p>
+          <p className={styles.basic}>Y:
+            <input name='y' id='y' type='number' placeholder='Y' required/>
+          </p>
+          <p className={styles.basic}>Z:
+            <input name='z' id='z' type='number' placeholder='Z' required/>
+          </p>
+          <p className={styles.basic}>Yaw:
+            <input name='yaw' id='yaw' type='number' placeholder='Yaw' required/>
+          </p>
+          <p className={styles.basic}>Pitch:
+            <input name='pitch' id='pitch' type='number' placeholder='Pitch' required/>
+          </p>
+          <p className={styles.basic}>Roll:
+            <input name='roll' id='roll' type='number' placeholder='Roll' required/>
+          </p>
+          <button>
+            Save Changes
+          </button>
           <button onClick={() => setLoop(curr => !curr)}>
             Loop? (Temp)
           </button>
-        </header>
+        </header> 
       </div>
       
     </div>

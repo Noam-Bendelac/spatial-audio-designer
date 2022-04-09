55068b4f0e8149281ff6ecda953d270e2431fd33
sjl18 <sjl18@vt.edu>
2022-04-09 09:43:28 -0400

    cone

--- src/scene3d/Scene.tsx
+++ src/scene3d/Scene.tsx
@@ -20,6 +20,7 @@ export const Scene = ({
       {/* <Box position={[-1.2, 0, 0]} />
       <Box position={[1.2, 0, 0]} /> */}
       <GizmoCyl position={[1, 0, 0]}/>
+      <GizmoCone position={[-1, 0, 0]}/>
     </Canvas>
   </div>
 }
@@ -77,36 +78,36 @@ const GizmoCyl = (props: MeshProps) => {
       onPointerOut={() => setHovered(false)}
     >
       <cylinderGeometry args={[.25, .25, 1, 20, 1]} />
-      <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
+      <meshStandardMaterial color={hovered ? 'hotpink' : 'green'} />
     </mesh>
   )
 }
 
-// //messing around
-// const GizmoCone = (props: MeshProps) => {
-//   const ref = useRef<Mesh>(null!)
+//messing around
+const GizmoCone = (props: MeshProps) => {
+  const ref = useRef<Mesh>(null!)
 
-//   const [hovered, setHovered] = useState(false)
-//   const [clicked, setClicked] = useState(false)
+  const [hovered, setHovered] = useState(false)
+  const [clicked, setClicked] = useState(false)
   
-//   useFrame(() => {
-//     ref.current.rotation.x += 0.01
-//     ref.current.rotation.y += 0.01
-//   })
+  useFrame(() => {
+    ref.current.rotation.x += 0.01
+    ref.current.rotation.y += 0.01
+  })
   
-//   return (
-//     <mesh
-//       {...props}
-//       ref={ref}
-//       scale={clicked ? 1.5 : 1}
+  return (
+    <mesh
+      {...props}
+      ref={ref}
+      scale={clicked ? 1.5 : 1}
       
-//       onClick={() => setClicked(!clicked)}
-//       onPointerOver={() => setHovered(true)}
-//       onPointerOut={() => setHovered(false)}
-//     >
-//       <cylinderGeometry args={[.25, .25, 1, 20, 1]} />
-//       <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
-//     </mesh>
-//   )
-// }
+      onClick={() => setClicked(!clicked)}
+      onPointerOver={() => setHovered(true)}
+      onPointerOut={() => setHovered(false)}
+    >
+      <coneGeometry args={[.5, .75, 24, 1]} />
+      <meshStandardMaterial color={hovered ? 'hotpink' : 'blue'} />
+    </mesh>
+  )
+}
 
